41c1237e52071cb8b9db2c5ba0daae93ad32749d
JDAAR <jdanaconar@gmail.com>
2024-06-27 23:06:28 -0500

    Agregando animación a las cards buscando que ocaciona el movimiento indeseado

--- src/components/cards/FilterMenu.jsx
+++ src/components/cards/FilterMenu.jsx
@@ -5,13 +5,18 @@ const FilterButtons = ({ filter, handleFilterChange }) => {
 	// Definir los tipos de proyectos disponibles
 	const projectTypes = ["all", "react", "javascript", "python", "java", "sass"]; // Agrega más tipos según tus necesidades
 
+	const handleClick = (event, type) => {
+		event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+		handleFilterChange(type);
+	};
+
 	return (
 		<div className="filter-menu">
 			{projectTypes.map(type => (
 				<a
 					key={type}
 					href="#"
-					onClick={() => handleFilterChange(type)}
+					onClick={(event) => handleClick(event, type)}
 					className={filter === type ? "active" : ""}
 				>
 					{type.charAt(0).toUpperCase() + type.slice(1)} {/* Capitaliza el primer carácter */}
